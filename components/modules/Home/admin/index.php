cf619890651c1ba587cb74f421ce2e8822ed0494
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-22 18:43:11 +0200

    Users generation

---
+++ components/modules/Home/admin/index.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package		Home
+ * @category	modules
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
+ * @license		MIT License, see license.txt
+ */
+namespace	cs;
+use			h;
+$Group	= Group::instance();
+$Index	= Index::instance();
+$User	= User::instance();
+if (isset($_POST['group'], $_POST['count'])) {
+	$group	= $Group->get($_POST['group'], 'title');
+	$count	= (int)$_POST['count'];
+	while ($count--) {
+		for (
+			$i = 100;
+			$User->get_id(hash('sha224', $login	= $group.'_'.$i));
+			++$i
+		) {}
+		$new_user = $User->registration("$login@".DOMAIN, false, false);
+		$Index->content(
+			 h::p("Логін: $login Пароль: $new_user[password]")
+		);
+	}
+}
+$groups	= array_filter(
+	$Group->get_all(),
+	function ($group) {
+		return $group['id'] > 3;
+	}
+);
+Index::instance()->buttons	= false;
+$Index->content(
+	h::{'p.cs-center'}(
+		h::span('Група').
+		h::{'select[name=group]'}(
+			[
+				'in'	=> array_values(array_map(
+					function ($group) {
+						return $group['description'];
+					},
+					$groups
+				)),
+				'value'	=> array_values(array_map(
+					function ($group) {
+						return $group['id'];
+					},
+					$groups
+				))
+			]
+		)
+	).
+	h::{'p.cs-center'}(
+		h::span('Кількість').
+		h::{'input[name=count][type=number][min=1][value=1]'}()
+	).
+	h::{'p.cs-center button[type=submit]'}('Додати користувачів')
+);
