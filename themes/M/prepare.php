e2556183f6b9ff0b31a2ef855ad736cb38200ccb
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-22 14:19:32 +0200

    M theme

---
+++ themes/M/prepare.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package		ClevereStyle CMS
+ * @subpackage	CleverStyle theme
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2013-2014, Nazar Mokrynskyi
+ * @license		MIT License, see license.txt
+ */
+namespace	cs;
+if (preg_match('/msie|trident/i',$_SERVER['HTTP_USER_AGENT'])) {
+	Page::instance()->Head	.= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+}
