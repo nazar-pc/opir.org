ffa2e3581c146d68409e25ad523abb4321aa638d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-23 22:55:18 +0200

    Hide copyrights in js/css files

--- custom.php
+++ custom.php
@@ -5,4 +5,11 @@
  * For example, you can add here including of class, which has the same name as core system class,
  * in this case your class will be used (may be useful in certain cases when modification of system files is needed)
  */
+namespace	cs\custom;
 $_SERVER['REMOTE_ADDR']	= '127.0.0.1';
+class Page extends \cs\Page {
+	function css_includes_processing (&$data, $file) {
+		parent::css_includes_processing($data, $file);
+		$data	= str_replace(['Nazar', 'Mokrynskyi'], '', $data);
+	}
+}
