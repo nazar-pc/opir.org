26580fba8e93b0b2078b64069b28b97955254bdb
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-23 23:27:22 +0200

    Hiding author

--- core/traits/Singleton.php
+++ core/traits/Singleton.php
@@ -32,8 +32,9 @@ trait Singleton {
 		if (isset($instance)) {
 			return $instance;
 		}
-		if (substr(__CLASS__, 0, 2) == 'cs' && class_exists('cs\\custom\\'.substr(__CLASS__, 2), false)) {
-			$instance	= 'cs\\custom\\'.substr(__CLASS__, 2);
+		$class	= ltrim(get_called_class(), '\\');
+		if (substr($class, 0, 2) == 'cs' && class_exists('cs\\custom'.substr($class, 2), false)) {
+			$instance	= 'cs\\custom'.substr($class, 2);
 			$instance	= $instance::instance();
 		} else {
 			$instance	= new static;
--- custom.php
+++ custom.php
@@ -6,10 +6,50 @@
  * in this case your class will be used (may be useful in certain cases when modification of system files is needed)
  */
 namespace	cs\custom;
+use			cs\Language,
+			cs\Trigger;
 $_SERVER['REMOTE_ADDR']	= '127.0.0.1';
 class Page extends \cs\Page {
-	function css_includes_processing (&$data, $file) {
-		parent::css_includes_processing($data, $file);
-		$data	= str_replace(['Nazar', 'Mokrynskyi'], '', $data);
+	function init ($title, $keywords, $description, $theme, $color_scheme) {
+		$this->replace('<meta content="CleverStyle CMS by Mokrynskyi Nazar" name="generator">', '');
+		parent::init($title, $keywords, $description, $theme, $color_scheme);
+	}
+	protected function rebuild_cache () {
+		$key	= '';
+		Trigger::instance()->run(
+			'System/Page/rebuild_cache',
+			[
+				'key'	=> &$key
+			]
+		);
+		$this->get_includes_list(true);
+		foreach ($this->includes as $extension => &$files) {
+			$temp_cache = '';
+			foreach ($files as $file) {
+				if (file_exists($file)) {
+					$current_cache = file_get_contents($file);
+					if ($extension == 'css') {
+						/**
+						 * Insert external elements into resulting css file.
+						 * It is needed, because those files will not be copied into new destination of resulting css file.
+						 */
+						$this->css_includes_processing($current_cache, $file);
+					}
+					if ($extension == 'js') {
+						$current_cache .= ';';
+					}
+					$temp_cache .= $current_cache;
+					unset($current_cache);
+				}
+			}
+			if ($extension == 'js') {
+				$temp_cache	= "window.cs.Language="._json_encode(Language::instance()).";$temp_cache";
+			}
+			$temp_cache	= str_replace(['Nazar', 'Mokrynskyi', 'nazar@mokrynskyi.com', 'CleverStyle CMS', 'MIT License, see license.txt'], '', $temp_cache);
+			file_put_contents(PCACHE."/$this->pcache_basename$extension", gzencode($temp_cache, 9), LOCK_EX | FILE_BINARY);
+			$key .= md5($temp_cache);
+		}
+		file_put_contents(PCACHE.'/pcache_key', mb_substr(md5($key), 0, 5), LOCK_EX | FILE_BINARY);
+		return $this;
 	}
 }
