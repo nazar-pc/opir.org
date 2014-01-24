6da4530621787f1d9e65569309a6276ca44d9a5a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-24 12:37:08 +0200

    Map type back to normal

--- components/modules/Home/includes/js/map.js
+++ components/modules/Home/includes/js/map.js
@@ -15,8 +15,7 @@
       window.map = new ymaps.Map('map', {
         center: [50.45, 30.523611],
         zoom: 13,
-        controls: ['typeSelector', 'zoomControl'],
-        type: 'yandex#hybrid'
+        controls: ['typeSelector', 'zoomControl']
       });
       categories = [];
       (function() {
