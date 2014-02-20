dea391c83149f062b42b548c670d6f2f518ae7fa
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-02-21 01:46:15 +0200

    New APIs

---
+++ components/modules/Home/api/categories_groups.get.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package		Home
+ * @category	modules
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
+ * @license		MIT License, see license.txt
+ */
+namespace	cs\modules\Home;
+use			cs\Page;
+Page::instance()->json(
+	Events_categories_groups::instance()->get_all()
+);
