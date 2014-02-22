5bf20bd51e90aa9cef74b057c21cb9f8962f19df
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-02-22 04:56:43 +0200

    Small fix

--- themes/M/prepare.php
+++ themes/M/prepare.php
@@ -8,7 +8,7 @@
  */
 namespace	cs;
 $Page	= Page::instance();
-if (preg_match('/msie|trident/i',$_SERVER['HTTP_USER_AGENT'])) {
+if (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/msie|trident/i',$_SERVER['HTTP_USER_AGENT'])) {
 	$Page->Head	.= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
 }
 $Page->Head	.= '<meta name="viewport" content="width=1200">';
