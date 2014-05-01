3555efc76d2595fef54c215e29ef38dbde0bfabb
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-05-01 23:43:39 +0300

    No "Elections" in page title

--- components/modules/Elections/index.php
+++ components/modules/Elections/index.php
@@ -1,33 +1,36 @@
 <?php
 /**
- * @package		Elections
- * @category	modules
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package        Elections
+ * @category       modules
+ * @author         Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright      Copyright (c) 2014, Nazar Mokrynskyi
+ * @license        MIT License, see license.txt
  */
-namespace	cs\modules\Home;
+namespace cs\modules\Home;
+
 use
 	h,
+	cs\Index,
 	cs\Language,
 	cs\Page,
 	cs\User,
 	cs\modules\Info\Info,
 	cs\modules\Help\Help;
 
-$L					= Language::instance();
-$Page				= Page::instance();
-$Page->Description	= 'opir.org - ми контролюємо вибори';
+Index::instance()->title_auto = false;
+$L                            = Language::instance();
+$Page                         = Page::instance();
+$Page->Description            = 'opir.org - ми контролюємо вибори';
 $Page->og('image', 'https://opir.org/components/modules/Home/includes/img/share.png');
 $Page->og('image:secure_url', 'https://opir.org/components/modules/Home/includes/img/share.png');
 $Page->link([
-	'rel'	=> 'image_src',
-	'href'	=> 'https://opir.org/components/modules/Home/includes/img/share.png'
+	'rel'  => 'image_src',
+	'href' => 'https://opir.org/components/modules/Home/includes/img/share.png'
 ]);
-$Page->Header	=
+$Page->Header =
 	h::{'div.cs-elections-logo'}(
 		h::{'a[href=/] img'}([
-			'src'	=> "components/modules/Elections/includes/img/logo.png"
+			'src' => "components/modules/Elections/includes/img/logo.png"
 		])/*.
 		'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="facebook" data-yashareLink="https://www.facebook.com/opir.org" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Home/includes/img/share.png"></div>'.
 		'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="vkontakte,twitter" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Home/includes/img/share.png"></div>'*/
