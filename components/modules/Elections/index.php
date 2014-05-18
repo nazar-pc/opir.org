dd225e0261f58b809d6c47d81324ae246e1eff51
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-05-18 05:49:56 +0300

    Sidebar prototype (search doesn't work) yet and that is why temporary hidden.

--- components/modules/Elections/index.php
+++ components/modules/Elections/index.php
@@ -21,19 +21,17 @@ Index::instance()->title_auto = false;
 $L                            = Language::instance();
 $Page                         = Page::instance();
 $Page->Description            = 'opir.org - ми контролюємо вибори';
-$Page->og('image', 'https://opir.org/components/modules/Home/includes/img/share.png');
-$Page->og('image:secure_url', 'https://opir.org/components/modules/Home/includes/img/share.png');
+$Page->og('image', 'https://opir.org/components/modules/Elections/includes/img/share.png');
+$Page->og('image:secure_url', 'https://opir.org/components/modules/Elections/includes/img/share.png');
 $Page->link([
 	'rel'  => 'image_src',
-	'href' => 'https://opir.org/components/modules/Home/includes/img/share.png'
+	'href' => 'https://opir.org/components/modules/Elections/includes/img/share.png'
 ]);
 $Page->Header =
 	h::{'div.cs-elections-logo'}(
 		h::{'a[href=/] img'}([
 			'src' => "components/modules/Elections/includes/img/logo.png"
-		])/*.
-		'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="facebook" data-yashareLink="https://www.facebook.com/opir.org" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Home/includes/img/share.png"></div>'.
-		'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="vkontakte,twitter" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Home/includes/img/share.png"></div>'*/
+		])
 	).
 	h::{'nav.cs-elections-switch-language'}(
 		h::span(
@@ -68,5 +66,49 @@ $Page->content(
 	).
 	h::{'section.cs-elections-help-initiative-modal[style=display:none] article'}(
 		Help::get()
+	).
+	h::{'aside.cs-elections-main-sidebar'}(
+		h::{'div.cs-elections-socials'}(
+			'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="facebook" data-yashareLink="https://www.facebook.com/opir.org" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Elections/includes/img/share.png"></div>'.
+			'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="vkontakte,twitter" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Elections/includes/img/share.png"></div>'
+		).
+//		h::h2($L->precincts_search).
+//		h::{'input.cs-elections-precincts-search[type=search]'}([
+//			'placeholder'	=> $L->number_or_address
+//		]).
+		h::h2($L->mobile_apps).
+		h::{'div.cs-elections-mobile-apps'}(
+			h::a(
+				$L->download_in('App Store'),
+				[
+					'onclick'	=> "$.cs.simple_modal('$L->soon')"
+				]
+			).
+			h::a(
+				$L->download_in('Google Play'),
+				[
+					'onclick'	=> "$.cs.simple_modal('$L->soon')"
+				]
+			).
+			h::a(
+				$L->download_in('Market Place'),
+				[
+					'onclick'	=> "$.cs.simple_modal('$L->soon')"
+				]
+			)
+		).
+		h::h2($L->contacts).
+		h::{'div.cs-elections-contacts'}(
+			h::a(
+				h::icon('phone').'+380 93 01 222 11').
+			h::{'a[href=mailto:info@opir.org]'}(
+				h::icon('envelope').'info@opir.org'
+			)
+		).
+		h::h2($L->map_legend).
+		h::{'div.cs-elections-map-legend'}(
+			h::div($L->precincts_with_violations).
+			h::div($L->precincts_without_violations)
+		)
 	)
 );
