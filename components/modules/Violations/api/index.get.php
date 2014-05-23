c5ffb424825cccc631cd5089fda392e1af7ab95a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-05-23 03:30:41 +0300

    Fixed parameters for api/Violations

--- components/modules/Violations/api/index.get.php
+++ components/modules/Violations/api/index.get.php
@@ -12,9 +12,9 @@ use cs\Page;
 
 $Page       = Page::instance();
 $Violations = Violations::instance();
-$number     = isset($_POST['number']) ? min((int)$_POST['number'], 100) : 10;
+$number     = isset($_GET['number']) ? min((int)$_GET['number'], 100) : 10;
 $number     = max($number, 0);
-$last_id    = isset($_POST['last_id']) ? max((int)$_POST['last_id'], 0) : 0;
+$last_id    = isset($_GET['last_id']) ? max((int)$_GET['last_id'], 0) : 0;
 $Page->json(
 	$Violations->last_violations($number, $last_id) ?: []
 );
