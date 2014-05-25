4ac0c50232dc447020f2a38ddc861da0075cb7a8
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-05-25 15:15:15 +0300

    Yandex Share always for general opir.org url

--- components/modules/Elections/index.php
+++ components/modules/Elections/index.php
@@ -54,7 +54,7 @@ $Page->content(
 	h::{'aside.cs-elections-main-sidebar'}(
 		h::{'div.cs-elections-socials'}(
 			'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="facebook" data-yashareLink="https://www.facebook.com/opir.org" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Common/includes/img/share.png"></div>'.
-			'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="vkontakte,twitter" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Common/includes/img/share.png"></div>'
+			'<div class="yashare-auto-init" data-yashareL10n="uk" data-yashareQuickServices="vkontakte,twitter" data-yashareLink="https://opir.org" data-yashareTheme="counter" data-yashareImage="https://opir.org/components/modules/Common/includes/img/share.png"></div>'
 		).
 		h::h2($L->precincts_search).
 		h::{'input.cs-elections-precincts-search[type=search]'}([
