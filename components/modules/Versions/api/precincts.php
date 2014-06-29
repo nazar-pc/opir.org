e88e4bb6691e7e3eee8f35e0670dff629109ae9b
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-06-30 00:28:24 +0200

    Versions module

---
+++ components/modules/Versions/api/precincts.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package		Versions
+ * @category	modules
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
+ * @license		MIT License, see license.txt
+ */
+namespace cs;
+Page::instance()->json(2);
