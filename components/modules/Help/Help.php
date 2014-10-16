4b47f331266c350af8432241cd720eb2da48c690
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-10-16 15:05:58 +0200

    Transfer account update

--- components/modules/Help/Help.php
+++ components/modules/Help/Help.php
@@ -26,7 +26,7 @@ class Help {
 					<p>ЄДРПОУ: 39214344</p>
 					<p>Р\р: 26001510692100</p>
 					<p>Отримувач: ГО ВГО ОПІГОРГ</p>
-					<p>МФО банку: 351005 (ПАТ "УкрСиббанк")</p>
+					<p>МФО банку: 351005 (АТ "УкрСиббанк")</p>
 					<p>Призначення: Добровільна пожертва на здійснення статутної діяльності ГО "ВГО ОПІРОРГ".</p>
 					<p>Гаманець Bitcoin: 1F8Wq9t562hHGcXfSDK7ZUZJhUjroZhwRg</p>
 					<p>Від Вашої допомоги напряму залежить кількість спостерігачів-стрімерів, що в свою чергу напряму впливає на об’єм громадського контролю над чесністю виборів в Україні!</p>
@@ -36,11 +36,11 @@ class Help {
 					<p>На данный момент opir.org запускает проект "Глаза Украины" - организация процесса стрим-видеонаблюдения с избирательных участков, разработка веб-платформы для сбора информации о нарушениях и разработка мобильного программного обеспечения.</p>
 					<p>Проект является социальным но не коммерческим и мы нуждаемся в вашей помощи</p>
 					<p>Счёт для перевода в UAH/EUR/USD:</p>
-					<p>ЄДРПОУ: 39214344</p>
-					<p>Р\р: 26001510692100</p>
-					<p>Отримувач: ГО ВГО ОПІГОРГ</p>
-					<p>МФО банку: 351005 (ПАТ "УкрСиббанк")</p>
-					<p>Призначення: Добровільна пожертва на здійснення статутної діяльності ГО "ВГО ОПІРОРГ".</p>
+					<p>ЕГРПОУ: 39214344</p>
+					<p>Р\с: 26001510692100</p>
+					<p>Получатель: ГО ВГО ОПІГОРГ</p>
+					<p>МФО банка: 351005 (АО "УкрСиббанк")</p>
+					<p>Назначение: Добровольное пожертвование на осуществление уставной деятельности ГО "ВГО ОПИРОРГ".</p>
 					<p>Кошелек Bitcoin: 1F8Wq9t562hHGcXfSDK7ZUZJhUjroZhwRg</p>
 					<p>От Вашей помощи напрямую зависит количество наблюдателей-стримеров, что в свою очередь прямую влияет на объем общественного контроля над честностью выборов в Украине!</p>
 				';
@@ -50,11 +50,11 @@ class Help {
 					<p>opir.org is launching "Eyes of Ukraine" project for video stream of surveillance process on polling stations, the development of web-based platform to gather information on violations and mobile software development.</p>
 					<p>The project is social and not commercial so we need your help.</p>
 					<p>Account for transfer in UAH/EUR/USD:</p>
-					<p>ЄДРПОУ: 39214344</p>
-					<p>Р\р: 26001510692100</p>
-					<p>Отримувач: ГО ВГО ОПІГОРГ</p>
-					<p>МФО банку: 351005 (ПАТ "УкрСиббанк")</p>
-					<p>Призначення: Добровільна пожертва на здійснення статутної діяльності ГО "ВГО ОПІРОРГ".</p>
+					<p>EDRPOU: 39214344</p>
+					<p>Correspondent account: 26001510692100</p>
+					<p>Recipient: ГО ВГО ОПИРОРГ</p>
+					<p>MFO: 351005 (JSC UkrSibbank)</p>
+					<p>Purpose: Donation to carry out statutory activities NGO "APA OPIRORG".</p>
 					<p>Bitcoin Wallet: 1F8Wq9t562hHGcXfSDK7ZUZJhUjroZhwRg</p>
 					<p>The number of observers-streamers directly depends on your help, which in turn directly affects the amount of public control over the integrity of elections in Ukraine!</p>
 				';
