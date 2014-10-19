3382fe6c20c4e2b6de00a0d020d65deec20cd8fd
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-10-20 00:02:00 +0200

    Help block updated

--- components/modules/Help/Help.php
+++ components/modules/Help/Help.php
@@ -20,43 +20,37 @@ class Help {
 		switch (Language::instance()->clang) {
 			case 'uk':
 				return '
-					<p>Наразі opir.org запускає проект "Очі України" – організація процесу стрім-відео спостереження з виборчих дільниць, розробка веб-платформи для збору інформації щодо порушень та розробка мобільного програмного забезпечення</p>
-					<p>Проект являється соціальним та не комерційним тож ми потребуємо вашої допомоги.</p>
+					<p>Принципово вектор розвитку і діяльності нашої організації збігається з інтересами народу України. Ми зобов\'язуємося й надалі відстоювати інтереси громади та лишатися незалежною організацєю. Саме тому ми потребуємо Вашої фінансової підтримки в нашого проекту.</p>
+					<p>Ми зобов’язуємося вести відкриту звітність щодо використання коштів, оскільки принцип чесності і прозорості лежить у фундаменті нашої організації.</p>
 					<p>Рахунок для переказу в UAH/EUR/USD:</p>
 					<p>ЄДРПОУ: 39214344</p>
 					<p>Р\р: 26001510692100</p>
 					<p>Отримувач: ГО ВГО ОПІГОРГ</p>
 					<p>МФО банку: 351005 (АТ "УкрСиббанк")</p>
 					<p>Призначення: Добровільна пожертва на здійснення статутної діяльності ГО "ВГО ОПІРОРГ".</p>
-					<p>Гаманець Bitcoin: 1F8Wq9t562hHGcXfSDK7ZUZJhUjroZhwRg</p>
-					<p>Від Вашої допомоги напряму залежить кількість спостерігачів-стрімерів, що в свою чергу напряму впливає на об’єм громадського контролю над чесністю виборів в Україні!</p>
 				';
 			case 'ru':
 				return '
-					<p>На данный момент opir.org запускает проект "Глаза Украины" - организация процесса стрим-видеонаблюдения с избирательных участков, разработка веб-платформы для сбора информации о нарушениях и разработка мобильного программного обеспечения.</p>
-					<p>Проект является социальным но не коммерческим и мы нуждаемся в вашей помощи</p>
+					<p>Принципиально вектор развития и деятельности нашей организации совпадает с интересами народа Украины. Мы обязуемся и впредь отстаивать интересы общества и оставаться независимой организаций. Именно поэтому мы нуждаемся Вашей финансовой поддержке нашего проекта.</p>
+					<p>Мы обязуемся вести открытую отчетность об использовании средств, поскольку принцип честности и прозрачности лежит в фундаменте нашей организации.</p>
 					<p>Счёт для перевода в UAH/EUR/USD:</p>
 					<p>ЕГРПОУ: 39214344</p>
 					<p>Р\с: 26001510692100</p>
 					<p>Получатель: ГО ВГО ОПІГОРГ</p>
 					<p>МФО банка: 351005 (АО "УкрСиббанк")</p>
 					<p>Назначение: Добровольное пожертвование на осуществление уставной деятельности ГО "ВГО ОПИРОРГ".</p>
-					<p>Кошелек Bitcoin: 1F8Wq9t562hHGcXfSDK7ZUZJhUjroZhwRg</p>
-					<p>От Вашей помощи напрямую зависит количество наблюдателей-стримеров, что в свою очередь прямую влияет на объем общественного контроля над честностью выборов в Украине!</p>
 				';
 			case 'en':
 			default:
 				return '
-					<p>opir.org is launching "Eyes of Ukraine" project for video stream of surveillance process on polling stations, the development of web-based platform to gather information on violations and mobile software development.</p>
-					<p>The project is social and not commercial so we need your help.</p>
+					<p>Fundamentally vector of development and activities of our organization coincides with the interests of the people of Ukraine. We pledge to continue to defend the interests of the community and stay independent organization. That\'s why we need your financial support in our project.</p>
+					<p>We pledge to keep an open reporting on the use of funds, as the principle of honesty and transparency lies at the foundation of our organization.</p>
 					<p>Account for transfer in UAH/EUR/USD:</p>
 					<p>EDRPOU: 39214344</p>
 					<p>Correspondent account: 26001510692100</p>
-					<p>Recipient: ГО ВГО ОПИРОРГ</p>
+					<p>Recipient: ГО ВГО ОПІГОРГ</p>
 					<p>MFO: 351005 (JSC UkrSibbank)</p>
 					<p>Purpose: Donation to carry out statutory activities NGO "APA OPIRORG".</p>
-					<p>Bitcoin Wallet: 1F8Wq9t562hHGcXfSDK7ZUZJhUjroZhwRg</p>
-					<p>The number of observers-streamers directly depends on your help, which in turn directly affects the amount of public control over the integrity of elections in Ukraine!</p>
 				';
 		}
 
