adda269057d41d0d2f28b5bd8538641133671a4c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-10-25 21:03:29 +0200

    Revert

--- components/modules/Elections/includes/js/disqus.coffee
+++ components/modules/Elections/includes/js/disqus.coffee
@@ -9,14 +9,17 @@
 initialized = false
 window.init_disqus = (disqus_identifier)->
 	if !initialized
+		window.disqus_identifier	= disqus_identifier
+		window.disqus_url			= 'https://opir.org/#' + disqus_identifier
 		dsq							= document.createElement('script')
 		dsq.type					= 'text/javascript'
 		dsq.async					= true
 		dsq.src						= '//' + disqus_shortname + '.disqus.com/embed.js';
 		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq)
 		initialized = true
-	DISQUS.reset
-		reload	: true
-		config	: ->
-			this.page.identifier	= disqus_identifier
-			this.page.url			= 'https://opir.org/#' + disqus_identifier
+	else
+		DISQUS.reset
+			reload	: true
+			config	: ->
+				this.page.identifier	= disqus_identifier
+				this.page.url			= 'https://opir.org/#' + disqus_identifier
--- components/modules/Elections/includes/js/disqus.js
+++ components/modules/Elections/includes/js/disqus.js
@@ -17,20 +17,23 @@
   window.init_disqus = function(disqus_identifier) {
     var dsq;
     if (!initialized) {
+      window.disqus_identifier = disqus_identifier;
+      window.disqus_url = 'https://opir.org/#' + disqus_identifier;
       dsq = document.createElement('script');
       dsq.type = 'text/javascript';
       dsq.async = true;
       dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      initialized = true;
+      return initialized = true;
+    } else {
+      return DISQUS.reset({
+        reload: true,
+        config: function() {
+          this.page.identifier = disqus_identifier;
+          return this.page.url = 'https://opir.org/#' + disqus_identifier;
+        }
+      });
     }
-    return DISQUS.reset({
-      reload: true,
-      config: function() {
-        this.page.identifier = disqus_identifier;
-        return this.page.url = 'https://opir.org/#' + disqus_identifier;
-      }
-    });
   };
 
 }).call(this);
